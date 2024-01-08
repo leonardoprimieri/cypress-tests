262b56f99884e1c69a4435cc80b8af412631669b
Leonardo M. Primieri <leo.primieri@gmail.com>
2024-01-08 20:36:04 -0300

    feat: broke a test

--- src/components/create-user-form/components/newsletter-input/newsletter-input.cy.tsx
+++ src/components/create-user-form/components/newsletter-input/newsletter-input.cy.tsx
@@ -7,7 +7,7 @@ describe("<NewsletterInput />", () => {
 
     cy.get('input[type="checkbox"]').check();
     cy.get(
-      'span:contains("By checking this box you agree to receive our newsletter")'
+      'span:contains("By checking this box you agree to receive our newslettera")'
     ).should("be.visible");
   });
 
